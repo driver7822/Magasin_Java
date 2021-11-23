dcf0611c50506e1fd8c6ce49a636263f83691fb6
Victor Lorfevre <victor.lorfevre@condorcet.be>
2021-11-23 10:46:50 +0100

    Ajout du CrossOrigin

--- src/main/java/be/condorcet/victorlorfevreprojet1/webservices/RestClient.java
+++ src/main/java/be/condorcet/victorlorfevreprojet1/webservices/RestClient.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*",exposedHeaders = "*")
 @RestController
 @RequestMapping("/clients")
 public class RestClient {
